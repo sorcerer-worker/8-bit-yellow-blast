b3f58f2827091636356681b9ade96fa3bd78942e
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-02 13:12:27 -0400

    feat: Add BattleManager and managers folder

---
+++ src/managers/BattleManager.ts
@@ -0,0 +1,21 @@
+import { User } from "../structures/User";
+
+class Stats {
+
+    user1: User;
+    user2: User;
+
+    constructor(user1: User, user2: User) {
+        this.user1 = user1;
+        this.user2 = user2;
+    }
+}
+
+class BattleManager extends Stats {
+
+    attack(user: string, name: string): string {
+        return `${user} attacked with ${name}`
+    }  
+}
+
+export { BattleManager };
