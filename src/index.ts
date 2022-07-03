b184a6541265696c662b511b6808b11b079045aa
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-03 11:22:59 -0400

    chore: Clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { BattleManager } from "./managers/BattleManager";
 import { User, UserData } from "./structures/User";
 
-// function Battle(userData: UserData): BattleManager{
-//     const newUser = new User({name: "Juice"});
-//     return new BattleManager(newUser, newUser);
-// }
-
 class Battle extends BattleManager {
+    constructor(user1: UserData, user2: UserData) {
+        super(user1, user2);
 
+    }
 }
 
 export { Battle, BattleManager };
