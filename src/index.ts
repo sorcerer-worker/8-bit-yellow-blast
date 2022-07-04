cca86a956f72af453cfb117c62947e508f9f86b0
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-03 22:14:08 -0400

    feat: Add functionality of ConfigManager.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { BattleManager } from "./managers/BattleManager";
-import { User, UserData } from "./structures/User";
+import { ConfigManager, Configuration } from "./managers/ConfigManager"
+import { UserData } from "./structures/User";
 
+function isUserData(user: UserData | any): user is UserData {
+    return (user as UserData).name !== undefined;
+}
 class Battle extends BattleManager {
-    constructor(user1: UserData, user2: UserData) {
-        super(user1, user2);
+
+    constructor(user1: UserData | object, user2: UserData| object, config: Configuration | undefined = undefined) {
+        let usr1: UserData;
+        let usr2: UserData;
+        if (config) {
+            const newConfig = new ConfigManager(config, user1, user2);
+            usr1 = newConfig.user1;
+            usr2 = newConfig.user2; 
+        }
+        else if (isUserData(user1) && isUserData(user2)) {
+            usr1 = user1;
+            usr2 = user2;
+        }
+        else {
+            throw new Error("Invalid format unable to use the UserData");
+        }
+        if (!isUserData(user1) || !isUserData(user2)) {
+            throw new Error("Invalid format unable to use the UserData");
+        }
+        super(usr1, usr2);
 
     }
 }
