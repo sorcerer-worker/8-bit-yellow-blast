cf5e84f336d631fff1344577761c9e9525f8ff1b
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-08 13:38:40 -0400

    feat: Add required id to turns manager

--- src/managers/TurnsManager.ts
+++ src/managers/TurnsManager.ts
@@ -1,11 +1,20 @@
 import { User, UserData } from "../structures/User";
 
+type UserId = number;
+
+type UserWithPosition = [
+    string,
+    number
+]
+
 class TurnsManager {
 
-    turnTable: Map<number, string>;
-    currentTurn: number;
+    turnTableRef: Map<UserId, UserWithPosition> // Adds additional reference to fixed position
+    turnTable: Map<number, string>; // Fixed Positions
+    currentTurn: number; // Goes through fixed positions
 
     constructor() {
+        this.turnTableRef = new Map();
         this.turnTable = new Map();
         this.currentTurn = 0;
     }
@@ -17,9 +26,27 @@ class TurnsManager {
     }
     add(user: UserData): number{
         const count = this.turnTable.size
+        this.turnTableRef.set(user.id, [user.name, count])
         this.turnTable.set(count, user.name)
         return count;
     }
 }
 
 export { TurnsManager }
+
+/*
+
+Go into Battle system
+    GET turns bat.turns.getUser(ID) ALT || bat.turns.users.table
+        EACH USER
+            RETURN NAME
+    Go into Battle system # ?getUser(ID).moves
+        GET user USING NAME # Possibly ID
+            GET .moves
+
+
+= = (Battle)
+= = (User) = = (Turns) EACH CANNOT GET DATA FROM SAME LEVEL
+
+
+            */
