ba9ad600b60d2594643536228e55d63a28c121fb
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-08 13:48:50 -0400

    feat: Adjust convert utility tool for ids

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,17 +1,23 @@
 import { User, UserData } from "../structures/User";
 
 type Configuration = {
-    name: string
+    name: string,
+    id: string
 }
 
 function convert(user: any, config: Configuration): UserData {
     let newUserData: UserData = {
         name: config.name ? user[config.name] : user["name"],
+        id: config.id ? user[config.id] : user["id"],
         ...user // Add remaining info
     }
     if (config.name) {
         // Deletes duplicate
         delete newUserData[config.name]
+    }
+    if (config.id) {
+        // Deletes duplicate
+        delete newUserData[config.id]
     }
     return newUserData;
 }
