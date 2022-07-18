3aab91f3527e24a555507cd843163129afc39079
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-18 08:21:23 -0400

    feat: Add comments for additional information

--- src/structures/Move.ts
+++ src/structures/Move.ts
@@ -1,14 +1,21 @@
-class Move {
+/**
+ * Represents each `Move` used by `User` objs.
+ */
+export class Move {
 
-    name: string;
-    rating: number;
-    type: string;
+    public name: string;
+    public rating: number;
+    public type: string;
     
+    /**
+     * Turns compatible data into `Move`
+     * @param {string} name Name of the move
+     * @param {number} rating Rating of move its strength
+     * @param {string} type Type of move it is
+     */
     constructor(name: string, rating: number, type: string) {
         this.name = name;
         this.rating = rating;
         this.type = type;
     }
 }
-
-export { Move }
