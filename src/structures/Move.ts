5b260bc6eac2483a4f0689392f818d2e4de83c78
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-18 21:58:46 -0400

    chore: Slightly change Move for cleaner code

--- src/structures/Move.ts
+++ src/structures/Move.ts
@@ -1,19 +1,25 @@
+export enum MoveType {
+    Heals,
+    Damages,
+    Restores // Possibly will be removed as mentioned in ActionManager
+} 
+
 /**
- * Represents each `Move` used by `User` objs.
+ * Represents each `Move` used by `User` objects.
  */
 export class Move {
 
     public name: string;
     public rating: number;
-    public type: string;
+    public type: MoveType;
     
     /**
      * Turns compatible data into `Move`
      * @param {string} name Name of the move
      * @param {number} rating Rating of move its strength
-     * @param {string} type Type of move it is
+     * @param {MoveType} type Type of move it is
      */
-    constructor(name: string, rating: number, type: string) {
+    constructor(name: string, rating: number, type: MoveType) {
         this.name = name;
         this.rating = rating;
         this.type = type;
