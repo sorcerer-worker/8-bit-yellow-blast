e4a4f61682b548a175d8f58dc03f7eef0b7c23d5
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-18 22:03:30 -0400

    feat: Add tests for action manager

---
+++ src/managers/ActionManager.test.ts
@@ -0,0 +1,36 @@
+
+import { MoveData, ActionManager } from "./ActionManager"
+import { Move, MoveType } from "../structures/Move";
+import { User } from "../structures/User";
+
+import mockAltUser from "../__mock__/mockAltUser.json"
+
+const moves: MoveData[] = [
+    {name: "Punch", rating :100, type: MoveType.Damages},
+    {name: "Heal", rating :100, type: MoveType.Heals},
+]
+
+let manager: ActionManager;
+
+describe("action manager", () => {
+    beforeAll(() => {
+        manager = new ActionManager(moves);
+    })
+    it("can make instance", () => {
+        expect(manager).toBeInstanceOf(ActionManager)
+    })
+    it("can change setting", () => {
+        const userInstance = new User(mockAltUser);
+        userInstance
+            .prevent
+            .overload
+            .hp = false;
+        expect(userInstance.prevent.overload.hp).toBeFalsy()
+    })
+    it("can use a particular Move on a User", () => {
+        const userInstance = new User(mockAltUser);
+        manager
+            .use(1)
+            .blast(userInstance)
+    })
+})
