12d851ec9e08ca89d7070a7ac0bac3c082e7ec23
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-18 22:27:21 -0400

    feat: Add comments as docs for battle manager

--- src/managers/BattleManager.ts
+++ src/managers/BattleManager.ts
@@ -1,21 +1,36 @@
 import { User, UserData } from "../structures/User";
 import { TurnsManager } from "./TurnsManager"
 
+/**
+ * Base class for `BattleManager` it keeps track of Stat information.
+ */
 class Stats {
 
     user1: User;
     user2: User;
-
+    /**
+     * Uses two copies of `UserData` object to contain the stat-related data for the two.
+     * @param {UserData} user1  The first `User`
+     * @param {UserData} user2  The second `User`
+     */
     constructor(user1: UserData, user2: UserData) {
         this.user1 = new User(user1);
         this.user2 = new User(user2);
     }
 }
 
+/**
+ * Manages battles and imports many thing required for such.
+ */
 class BattleManager extends Stats {
 
     turns: TurnsManager;
 
+    /**
+     * Uses two copies of `UserData` object to generate a battle between the two.
+     * @param {UserData} user1  The first `User`
+     * @param {UserData} user2  The second `User`
+     */
     constructor(user1: UserData, user2: UserData) {
         super(user1, user2)
         this.turns = new TurnsManager();
